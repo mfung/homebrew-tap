8c8864b462f1aa3fa3c9c7806fee7bc3fd36cc59
Meng F. <meng.fung@gmail.com>
2026-08-11 08:55:50 -0700

    Bump dottyenv to 0.2.1
    Hashes regenerated from the release SHA256SUMS.

--- Formula/dottyenv.rb
+++ Formula/dottyenv.rb
@@ -5,23 +5,23 @@ class Dottyenv < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/mfung/dottyenv/releases/download/v0.2.0/dottyenv-0.2.0-aarch64-apple-darwin.tar.gz"
-      sha256 "cacfb2b2c2a828aa919f7dfe1fc140ad0114a956d539255b05a5e43bdbbd57da"
+      url "https://github.com/mfung/dottyenv/releases/download/v0.2.1/dottyenv-0.2.1-aarch64-apple-darwin.tar.gz"
+      sha256 "90dd7a7f70811e201b8049b8ca42eb894c37cdc1abfa04eaefa4929402286955"
     end
     on_intel do
-      url "https://github.com/mfung/dottyenv/releases/download/v0.2.0/dottyenv-0.2.0-x86_64-apple-darwin.tar.gz"
-      sha256 "3cc81b810ce7a60b99e970f454cf42e40e91d77ce18babf168484b1e3a621336"
+      url "https://github.com/mfung/dottyenv/releases/download/v0.2.1/dottyenv-0.2.1-x86_64-apple-darwin.tar.gz"
+      sha256 "c4e3267537d99fd3bad91893f44c967ca13b370c170529acee4b4ba2e43e0a30"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/mfung/dottyenv/releases/download/v0.2.0/dottyenv-0.2.0-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "c8c83407a97d4ea52629e3c198fe741e4c34cf2b8affb33beb06ab212fb81a30"
+      url "https://github.com/mfung/dottyenv/releases/download/v0.2.1/dottyenv-0.2.1-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "eb9a20fb68a4f05f19e768357842f8dcf70cf54af1c6ce32f2faa26a9e4f5f03"
     end
     on_intel do
-      url "https://github.com/mfung/dottyenv/releases/download/v0.2.0/dottyenv-0.2.0-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "3d549b7263fc41c843d7cf2dd7a9f13163b5a537523ce94102a4acade2bd5e46"
+      url "https://github.com/mfung/dottyenv/releases/download/v0.2.1/dottyenv-0.2.1-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "31beb1054f6013206498f3a471ade9637f10093eb6fd1d2b6d73f37938267e9c"
     end
   end
 
